474d34c796461296a065ab4618b2de651d0f6f62
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2025-06-22 13:15:06 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,23 +1,18 @@
-#
-# Copyright (C) 2015 The Android Open-Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-ifneq ($(filter gtel3g,$(TARGET_DEVICE)),)
-
-LOCAL_PATH := $(call my-dir)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
-
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+    KERNEL_SRC :=
+else
+    KERNEL_SRC := $(TARGET_PREBUILT_KERNEL)
 endif
+
+ifeq ($(BOARD_PREBUILT_DT_IMAGE),)
+    DTB_SRC :=
+else
+    DTB_SRC := $(BOARD_PREBUILT_DT_IMAGE)
+endif
+
+# Правила для копирования ядра и DTB
+$(PRODUCT_OUT)/kernel: $(KERNEL_SRC)
+    cp $^ $@
+
+$(PRODUCT_OUT)/dt.img: $(DTB_SRC)
+    cp $^ $@
