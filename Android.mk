78c4d07da6ae92be9d18fe2e0ab20b5f91fa16e9
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2025-06-22 18:08:26 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,18 +1,25 @@
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-    KERNEL_SRC :=
-else
-    KERNEL_SRC := $(TARGET_PREBUILT_KERNEL)
+# Явное переопределение, если переменные окружения установлены.  
+# Если TARGET_PREBUILT_KERNEL существует, используем его значение, иначе остается пустая строка.
+KERNEL_SRC := $(strip $(TARGET_PREBUILT_KERNEL))
+DTB_SRC := $(strip $(BOARD_PREBUILT_DT_IMAGE))
+
+
+# Проверка, что исходные файлы ядра и DTB существуют.  Если нет - выдаем ошибку
+# stop - это функция make для остановки сборки с сообщением об ошибке.
+ifndef KERNEL_SRC
+$(warning "TARGET_PREBUILT_KERNEL is not defined. Kernel image will not be copied.")
 endif
 
-ifeq ($(BOARD_PREBUILT_DT_IMAGE),)
-    DTB_SRC :=
-else
-    DTB_SRC := $(BOARD_PREBUILT_DT_IMAGE)
+ifndef DTB_SRC
+$(warning "BOARD_PREBUILT_DT_IMAGE is not defined. DTB image will not be copied.")
 endif
+
 
 # Правила для копирования ядра и DTB
 $(PRODUCT_OUT)/kernel: $(KERNEL_SRC)
-    cp $^ $@  # Исправлено: добавлена табуляция перед cp
+    @echo "Copying kernel from $(KERNEL_SRC) to $@" # Отладочный вывод
+    $(if $(KERNEL_SRC), cp $(KERNEL_SRC) $@, @echo "Skipping kernel copy: KERNEL_SRC is empty") # Условное копирование
 
 $(PRODUCT_OUT)/dt.img: $(DTB_SRC)
-    cp $^ $@  # Исправлено: добавлена табуляция перед cp
+    @echo "Copying dt.img from $(DTB_SRC) to $@" # Отладочный вывод
+    $(if $(DTB_SRC), cp $(DTB_SRC) $@, @echo "Skipping dtb copy: DTB_SRC is empty") # Условное копирование
