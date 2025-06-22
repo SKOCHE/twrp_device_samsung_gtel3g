4db0395e2829daf046fcfcb46d5a4b8217273ce7
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2025-06-22 13:45:27 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -12,7 +12,7 @@ endif
 
 # Правила для копирования ядра и DTB
 $(PRODUCT_OUT)/kernel: $(KERNEL_SRC)
-    cp $^ $@
+    <ТАБУЛЯЦИЯ>cp $^ $@
 
 $(PRODUCT_OUT)/dt.img: $(DTB_SRC)
-    cp $^ $@
+    <ТАБУЛЯЦИЯ>cp $^ $@
